3431bef24affdabc5824ae5d13e4dae793b63411
Kalimaha <guido.barbaglia@gmail.com>
2015-05-25 10:54:46 +0200

    Adding examples to the main page.

--- src/main/webapp/js/codemirror-utils.js
+++ src/main/webapp/js/codemirror-utils.js
@@ -1,31 +1,36 @@
 if (!window.CMU) {
-	
-	window.CMU = {
-			
-		editor : null,
-		
-		init : function(textAreaID) {
-			var mime = 'text/x-mariadb';
-			if (window.location.href.indexOf('mime=') > -1) {
-				mime = window.location.href.substr(window.location.href.indexOf('mime=') + 5);
-			} 
-			CMU.editor = CodeMirror.fromTextArea(document.getElementById(textAreaID), {
-				mode : mime,
-				lineNumbers : true,
-				tabSize : 3,
-				lineWrapping : true,
-				styleActiveLine: true,
-				indentWithTabs: true,
-				smartIndent: true,
-				matchBrackets : true,
-				autofocus: true 
-			});
-		},
-		
-		getValue : function() {
-			return CMU.editor.getValue();
-		}
-			
-	};
-	
+
+    window.CMU = {
+
+        editor: null,
+
+        init: function (textAreaID) {
+            var mime = 'text/x-mariadb';
+            if (window.location.href.indexOf('mime=') > -1) {
+                mime = window.location.href.substr(window.location.href.indexOf('mime=') + 5);
+            }
+            CMU.editor = CodeMirror.fromTextArea(document.getElementById(textAreaID), {
+                mode: mime,
+                lineNumbers: true,
+                tabSize: 3,
+                lineWrapping: true,
+                styleActiveLine: true,
+                indentWithTabs: true,
+                smartIndent: true,
+                matchBrackets: true,
+                autofocus: true
+            });
+        },
+
+        getValue: function () {
+            return CMU.editor.getValue();
+        },
+
+        setValue: function (value) {
+            CMU.editor.setValue(value);
+            CMU.editor.autoFormatRange(0, 10);
+        }
+
+    };
+
 }
